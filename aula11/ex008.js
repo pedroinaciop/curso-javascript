60f79c36b47dd42d6b62fe68cbcffeee549d28ec
Pedro Inácio Penha dos Santos <inaciopedro2004@gmail.com>
2023-05-18 21:51:27 -0300

    Create ex011.html

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { // Condição simples!
+    console.log('Você ultrapassou o limite de velocidade, MULTADO!')
+}
+console.log('Dirige sempre com o cinto de segurança!')
