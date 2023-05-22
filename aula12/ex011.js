208be224981574d43df4c1ee147ac42200a47dea
Pedro Inácio Penha dos Santos <inaciopedro2004@gmail.com>
2023-05-22 17:12:40 -0300

    Create aula12

---
+++ aula12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 11
+if (idade < 16) {
+    console.log(`Você tem ${idade} anos.`)
+} else if (idade >= 16 && idade < 18 || idade > 65) {
+    console.log(`Você tem ${idade} anos o voto é "OPCIONAL".`) 
+}  else {
+    console.log(`Você tem ${idade} anos o voto é "OBRIGATÓRIO".`)
+}
---
+++ aula12/ex013.js
@@ -0,0 +1,6 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+/*
+
+*/
+console.log(diaSem)
