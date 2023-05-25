7e5bac00dd101c28dee3d214ec02881fd3ed8922
Pedro Inácio Penha dos Santos <inaciopedro2004@gmail.com>
2023-05-25 20:51:27 -0300

    Create ex14

--- ex014/script.js
+++ ex014/script.js
@@ -1,1 +1,24 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    var hora = 18
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+   
+    if (hora >= 0 && hora <= 12) {
+        //BOM DIA!
+        img.src = 'manha.jpg'
+        document.body.style.background = '#e1390b'
+    } else if (hora > 12 && hora < 18) {
+        //BOA TARDE!
+        img.src = 'tarde.jpg'
+        document.body.style.background = '#9890204c'
+    } else {
+        //BOA NOITE!
+        img.src = 'noite.jpeg'
+        document.body.style.background = '#515154'
+    }
+}
