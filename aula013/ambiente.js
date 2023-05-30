a5399981157e2f9b62ce0c34dc7d522c29cc58fd
Pedro Inácio Penha dos Santos <inaciopedro2004@gmail.com>
2023-05-30 19:22:51 -0300

    Create aula013

---
+++ aula013/ambiente.js
@@ -0,0 +1,25 @@
+/* LAÇO DE REPETIÇÃO COM TESTE LÓGICO NO INICIO(WHILE)
+
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c = c + 1
+}
+
+*/
+
+/* LAÇO DE REPETIÇÃO COM TESTE LÓGICO NO FINAL (DO WHILE)
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c = c + 1
+} while (c <= 6)  
+
+*/
+
+console.log('Vai começar...')
+for ( var c = 1;c <= 4;c++) {
+    console.log(c)  
+} 
+console.log('FIM!')
