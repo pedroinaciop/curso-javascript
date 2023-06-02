de9262f193cc30bca407f4901647548451e8e368
Pedro Inácio Penha dos Santos <inaciopedro2004@gmail.com>
2023-06-01 22:27:15 -0300

    .

--- aula014/ex017/modelo/aula14ex/script.js
+++ aula014/ex017/modelo/aula14ex/script.js
@@ -1,20 +1,18 @@
-var number = document.getElementById('txtnumber')
-var list = document.getElementById('txtlist')
-var res = document.querySelector('div#res')
-
 function tabuada() {
-    var numero = Number(number.value)
+    var number = document.getElementById('txtnumber')
+    var tab = document.getElementById('seltab')
 
-
-    document.getElementById('txtlist').value = (`
-    ${numero} x 1 = ${numero * 1}
-    ${numero} x 2 = ${numero * 2}
-    ${numero} x 3 = ${numero * 3}
-    ${numero} x 4 = ${numero * 4}
-    ${numero} x 5 = ${numero * 5}
-    ${numero} x 6 = ${numero * 6}
-    ${numero} x 7 = ${numero * 7}
-    ${numero} x 8 = ${numero * 8}
-    ${numero} x 9 = ${numero * 9}
-    ${numero} x 10 = ${numero * 10}`)
+    if (number.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        var n = Number(number.value)
+        var c = 1
+        tab.innerHTML = ''
+        for (c; c < 11; c++) {
+            var item = document.createElement('option') 
+            item.text = `${n} x ${c} = ${n*c}` 
+            item.value = `tab${c}`  
+            tab.appendChild(item)    
+        }
+    }
 }
