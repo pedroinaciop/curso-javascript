d9b1e8c72229357a25446559ad800791f90d4c8b
Pedro Inácio Penha dos Santos <inaciopedro2004@gmail.com>
2023-06-01 21:32:48 -0300

    Update script

--- aula014/ex016/modelo/aula14ex/script.js
+++ aula014/ex016/modelo/aula14ex/script.js
@@ -1,29 +1,38 @@
-var inicio = window.document.getElementById('txtinicio')
-var fim = window.document.getElementById('txtfim')
-var passo = window.document.getElementById('txtpasso')
-var res = document.querySelector('div#res')
-
-function limpar() {
-    res.innerHTML = 'Contando: <br>'
-    document.getElementById('res').value = ''
-    document.getElementById('txtinicio').value = ''
-    document.getElementById('txtfim').value = ''
-    document.getElementById('txtpasso').value = ''
-}
-
 function contar() {
-    var start = Number(inicio.value)
-    var end = Number(fim.value)
-    var pass = Number(passo.value)
 
-    for (var c = start; c <= end; c += pass) {
-        if (c == null || pass == null || end == null) {
-            res.innerHTML = 'Impossível contar!'
-            document.getElementById('res').reset()
+    var inicio = window.document.getElementById('txtinicio')
+    var fim = window.document.getElementById('txtfim')
+    var passo = window.document.getElementById('txtpasso')
+    var res = document.querySelector('div#res')
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {   
+        res.innerHTML = 'Impossível contar'
+    } else {
+        var start = Number(inicio.value)
+        var end = Number(fim.value)
+        var pass = Number(passo.value)
+        if (pass <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            pass = 1
         }
-        document.querySelector('div#res').innerHTML += c + '<span>&#128073;&#127995;</span>'
+
+        res.innerHTML = 'Contando: <br>'
+        document.getElementById('res').value = ''
+        document.getElementById('txtinicio').value = ''
+        document.getElementById('txtfim').value = ''
+        document.getElementById('txtpasso').value = ''
+
+        if (start < end) { //Crescente
+            for (var c = start; c <= end; c += pass) {
+                res.innerHTML += c + '<span>&#128073;&#127995;</span>'
+            }
+        } else { //Decrescente
+            for (var c = start; c >= end; c -= pass) {
+                res.innerHTML += c + '<span>&#128073;&#127995;</span>'
+            }
+        }
+        res.innerHTML += '<span>&#127987;</span>'
     }
-    document.querySelector('div#res').innerHTML += '<span>&#127987;</span>'
 }
 
 
