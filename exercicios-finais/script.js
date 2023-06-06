b1b1f15fe59c067131442c8fbc8824060a3a21b5
Pedro Inácio Penha dos Santos <inaciopedro2004@gmail.com>
2023-06-06 16:38:32 -0300

    Feito

--- exercicios-finais/script.js
+++ exercicios-finais/script.js
@@ -1,32 +1,63 @@
-let totnum = 5
-let maiornum 
-let menornum
-let soma = 25
-let media = 5.312515
+let num = document.getElementById('inum')
+let tab = document.getElementById('flista')
+var res = window.document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true 
+    } else {
+        return false
+    }  
+}
+    
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
 
 function adicionar() {
-    let tab = document.getElementById('seltab')
-    let item = document.createElement('option') 
-    var num = document.getElementById('inum')
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.` 
+        flista.appendChild(item)
+        res.innerHTML = ''
+
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista')
+    }
+    num.value = ''
+    num.focus()
+}   
     
-    var num = Number(num.value)
-
-     for (let i = 0; i < 10; i++) {
-        let myArray = [i]
-        item.text = `Valor ${myArray[i]} adicionado.` 
-        item.value = `${myArray[i]}` 
-        tab.appendChild(item)
-     }
-
-}
-
 function finalizar() {
-    var res = window.document.getElementById('res')
-
-    res.innerHTML = `<p>Ao todo, temos ${totnum} números cadastrados
-                    <p>O maior valor informado foi ${maiornum(Math.max(...myArray))}
-                    <p>O menor valor informado foi ${menornum(Math.min(...myArray))}
-                    <p>Somando todos os valores, temos ${soma}
-                    <p>A média dos valores digitados é ${media}.</p>`
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+        } else {
+            let tot = valores.length
+            let maior = valores[0]
+            let menor = valores[0]
+            let soma = 0
+            let media = 0 
+            for (let pos in valores) {
+                soma += valores[pos]
+            if (valores[pos] > maior) 
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]    
+            } 
+            media = soma / tot
+            res.innerHTML = ``
+            res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+            res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+            res.innerHTML += `<p>O maior valor informado foi ${menor}.</p>`
+            res.innerHTML += `<p>Somando todos os valores temos ${soma}.</p>`
+            res.innerHTML += `<p>A média doos valores digitados é ${media}.</p>`
+            
+                    
+    }
 }
-
