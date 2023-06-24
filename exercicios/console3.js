c1b7b7db9928cf977554e4fe57f8567744e1d072
Pedro Inácio Penha dos Santos <inaciopedro2004@gmail.com>
2023-06-23 21:01:59 -0300

    Praticando...

---
+++ exercicios/console3.js
@@ -0,0 +1,10 @@
+var variavel1 = 1;
+var variavel2 = 6;
+
+if (Math.abs(variavel1 - variavel2) <= 4) {
+    // As variáveis estão a até cinco números de distância
+    console.log("SIM");
+} else {
+    // As variáveis estão a uma distância maior do que cinco números
+    console.log("NÃO");
+}
